f3c8c50060b26f712776df02af250c4f3c68e6b8
Rinelsia <rinelsia@mail.ru>
2018-12-05 23:14:01 +0300

    Add files via upload

--- ISKRAENGINE/loader/loader.php
+++ ISKRAENGINE/loader/loader.php
@@ -4,10 +4,43 @@ $className = require_once __DIR__."\className.php";
 
 var_dump($className);
 // открывает файлы, пути описаны в className
+$opd = opendir(DIR);
+var_dump($opd);
+var_dump(dir(DIR));
+var_dump(scandir(DIR, 1));
 
-foreach ($className as $key => $value) {
-	# code...
-	include_once $value;
+$put = DIR;
+function Katalog ($kat){
+	$katalog_dir = scandir($kat, 1);
+	foreach ($katalog_dir as $key => $value) {
+		if($value=='..'){
+			break;
+		}else{
+			$DIR = $kat.'/'.$value;
+			var_dump(filetype($DIR));
+			echo "<br><p style='color:ff1200'>$DIR</p><br>";
+		}
+
+		// if(filetype($DIR) =="dir"){
+		// 	scandir($DIR, 1);
+		// }
+	}
+
+
 }
-app\controllers\controllers::controller();
+Katalog($put);
+
+
+$classes = spl_autoload_register(
+	function($class_name){
+		echo "<p style='color:ff1200'>$class_name</p>";
+	}
+
+);
+$cont = new controllers;
+// foreach ($className as $key => $value) {
+// 	# code...
+// 	include_once $value;
+// }
+// app\controllers\controllers::controller();
  ?>

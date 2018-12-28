2c6a3facca047b990c5b8058726176fd657024b8
Rinelsia <rinelsia@mail.ru>
2018-12-28 15:47:13 +0300

    Add files via upload

--- ISKRAENGINE/app/controllers/controllers.php
+++ ISKRAENGINE/app/controllers/controllers.php
@@ -1,31 +1,42 @@
 <?php 
 // namespace app\controllers;
-
+// $server_url - переменная полученая в modules/parseUrl.php. Имя адреса страницы
+// controller() - функция запуска классов для обработки запросов
 class controllers
 {
-	public $arr_view;// массив набора файлов view где ключ - имя страницы
+	public static $arr_view;// массив набора файлов view где ключ - имя страницы
 	public static $arr_key_url; // преременная названия вкладки страницы, или названия страницы. Является ключем массива подключаемых видов view
-	public static $arr_controller = [
-		"index" => $view = [
-			"menu"=>"menuView",
-			"content" => "contentView"
-		]
-	];
-	function controller() 
+	public static $arr_controller;
+
+	public function controller() 
 	{
 		echo "<br><p style='color:red'>Подключен Controller</p>";
-		$echo = new createView;
-		$echo->echoViewStr();
+		$this->MapController();
+		
 		(new parseURL)->globServ();
 		$this->viewController();
+		(new createView)->echoViewStr();
+
+		
 	}
+
 	public function viewController(){
-		self::$arr_key_url = parseURL::$server_url;
+		self::$arr_key_url = parseURL::$server_url;		
 		echo "url - ";
 		var_dump(self::$arr_key_url);
+		$file = self::$arr_view;
+		var_dump($file);
+		$arr_controller = self::$arr_view[self::$arr_key_url];
+		var_dump($arr_controller);
 	}
 
-
+	public function MapController(){
+		self::$arr_view = [
+			"index" => $view = [				
+				"content" => "contentView"
+			]
+		];
+	}
 	
 }
 
